9d2b587ae73e26098ca32e0ec35ba914a7dca1f7
Code-Monster1 <usupovaliakbar01@gmail.com>
2023-01-04 15:32:12 +0600

    about page full done

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,4 +19,31 @@ window.onclick = function(event) {
       }
     }
   }
-}
+};
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    740: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    920: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    },
+  }
+});
